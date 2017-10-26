5c1573a806761a9d0f2857b87daf9cd0b0d06620
Andreas Altendorfer <andreas@altendorfer.at>
2017-10-26 12:44:12 +0200

    Refactor Requester
    Rendering a 404 response is not a responsibility of the
    Requester.
    
    A new Endpoint `NotFound` was implemented, thus the
    Requester always creates an endpoint.

--- src/cc/iboard/backend/Requester.java
+++ src/cc/iboard/backend/Requester.java
@@ -3,7 +3,7 @@ package cc.iboard.backend;
 import cc.iboard.endpoints.Endpoint;
 import cc.iboard.endpoints.EndpointFactory;
 import cc.iboard.endpoints.Index;
-import cc.iboard.html.Html;
+import cc.iboard.endpoints.NotFound;
 
 /**
  *  <p>
@@ -12,14 +12,10 @@ import cc.iboard.html.Html;
  *  <p>
  *      The default Endpoint is `Index` for "/" and "".
  *  If a request-string is given, this path is passed to the `EndpointFactory` which
- *  returns an `Endpoint` for the given string.
+ *  returns an `Endpoint` for the given string. If no Endpoint defined for the given 
+ *  request-string a new `NotFound` endpoint will be created.
  *  <p>
- *      Finally, when an `Endpoint` was found, the `Requestor` calls `request()`
- *  on this `Endpoint` and returns the `Response`
- *  <p>
- *      If no Endpoint defined for the given request-string, a 404-response is
- *  created.
- *
+ *      Finally, the `Requestor` calls `request()` on this `Endpoint` and returns the `Response`
  *  <p>
  *  Example:
  *  <pre>{@code
@@ -32,44 +28,41 @@ import cc.iboard.html.Html;
  *  
  *  @see Endpoint
  *  @see Response
+ *  @see NotFound
  */
 class Requester {
 
     /**
-     * @param path    the first part of the path, eg `/index` is used to find the proper `Endpoint`.
-     * @param msg 
+     * @param path  the first part of the path, eg `/index` is used to find the proper `Endpoint`.
      * @return String the body returned from the `Endpoint.respond()` function.
+     * 
      * @see Endpoint
      * @see Requester
      */
     public Response request(String method, String path) {
-        Endpoint endpoint;
+        Endpoint endpoint = determineEndpoint(path);
         Request request = new Request(method,path);
 
+        return endpoint.respond(request);
+    }
+
+
+    // IMPLEMENTATION
+    
+    private Endpoint determineEndpoint(String path) {
+        Endpoint endpoint;
         if (isRootPath(path))
             endpoint = new Index();
         else try {
             endpoint = EndpointFactory.INSTANCE.createEndpoint(path);
         } catch (Exception e) {
-            return new Response(Response.HTTP_NOT_FOUND,render404(path));
+            endpoint = new NotFound();
         }
-
-        return endpoint.respond(request);
+        return endpoint;
     }
 
 	private boolean isRootPath(String path) {
 		return path.equals("/") || path.equals("");
-	}
-
-	private String render404(String path) {
-		return Html.html(
-			       Html.getHeader() +
-			       Html.body(
-			         Html.h1("404 - Page Not Found") +
-			         Html.p("There is no page at " + path + ".<br/> " +
-			           Html.a("Goto the Homepage", "/"))
-			          )
-			   );
 	}
 
 }
